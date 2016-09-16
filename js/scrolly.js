55fb18a438d162b17eac8621ace544b19ae05e05
mendez6412 <mendez6412@gmail.com>
2016-09-16 16:26:15 -0400

    added link to top

--- js/scrolly.js
+++ js/scrolly.js
@@ -1,11 +1,11 @@
 $('#navbar-items').on('activate.bs.scrollspy', function () {
   if ($('.test').hasClass('active')) {
-    console.log("yes")
     $('.magic').css('color', '#4682b4')
     $('.navbar-default .navbar-nav>.active>a').css('background-color', 'transparent')
   }
   else {
     $('.magic').css('color', '#f8f8f8')
+    $('.navbar-default .navbar-nav>.active>a').css('background-color', 'transparent')
   }
   $('.ignore').css({'visibility':'hidden'});
 })
